406972deca242c7691357d44a8b78eca4c027797
saywurdson <34385409+saywurdson@users.noreply.github.com>
2023-02-19 16:53:07 -0500

    update synpuf folder

---
+++ synpuf1/etl/load.py
@@ -0,0 +1,70 @@
+import duckdb, sys, os, dotenv, csv, re, dotenv
+import pandas as pd
+
+dotenv.load_dotenv(".env")
+
+BASE_OUTPUT_DIRECTORY = os.environ['BASE_OUTPUT_DIRECTORY']
+
+# create the connection
+con = duckdb.connect('/workspaces/practice/omop.db')
+print("Database connection established.")
+
+# create schema
+con.execute('create schema if not exists synpuf')
+
+# create metadata table
+con.execute('create table if not exists synpuf.logs (timestamp timestamp, message varchar)')
+
+# create tables
+tables = [
+    'care_site', 'condition_occurrence', 'death', 'device_cost',
+    'device_exposure', 'drug_cost', 'drug_exposure', 'location',
+    'measurement_occurrence', 'observation_period', 'observation',
+    'payer_plan_period', 'person', 'procedure_cost', 'procedure_occurrence',
+    'provider', 'specimen', 'visit_cost', 'visit_occurrence'
+]
+
+arg = sys.argv[1]
+while arg != 'overwrite' and arg != 'append':
+    print('Invalid argument. Please enter either "overwrite" or "append".')
+    arg = input()
+
+for table in tables:
+    for i in range(1, 21):
+        csv_file = f'{table}_{i}.csv'
+        file_path = f'/workspaces/practice/synpuf/data/BASE_OUTPUT_DIRECTORY/{csv_file}'
+        if os.path.exists(file_path) and file_path.endswith('.csv'):
+            print(f'Updating table: {table}...')
+            if arg == 'overwrite':
+                con.execute(f'create or replace table synpuf.{table} as select * from read_csv_auto("{file_path}", ignore_errors=1, all_varchar=True)')
+            elif arg == 'append':
+                con.execute(f'insert into synpuf.{table} select * from read_csv_auto("{file_path}", ignore_errors=1, all_varchar=True)')
+            print(f'Table {table} updated.')
+        else:
+            print(f'File {csv_file} not found or is not a csv file.')
+
+def extract_etl_stats(BASE_OUTPUT_DIRECTORY):
+    print('processing log files...')
+    pattern = re.compile(r'\[(.*?)\](.*)')
+    etl_stats_df = pd.DataFrame(columns=['Timestamp', 'Message'])
+    
+    for i in range(1, 21):
+        log_file_path = os.path.join(BASE_OUTPUT_DIRECTORY, f'etl_stats_{i}.txt')
+        if os.path.exists(log_file_path):
+            with open(log_file_path, 'r') as f:
+                for line in f:
+                    match = pattern.match(line)
+                    if match:
+                        etl_stats_df = pd.concat([etl_stats_df, pd.DataFrame({'Timestamp': [match.group(1)], 'Message': [match.group(2)]})], ignore_index=True)
+    return etl_stats_df
+
+# convert etl_stats_df to dataframe
+etl_stats_df = extract_etl_stats(BASE_OUTPUT_DIRECTORY)
+
+# use con.execute to add etl_stats_df to the metadata table
+print('Building logs table...')
+con.execute('insert into synpuf.logs select * from etl_stats_df')
+
+# close the connection
+con.close()
+print("Database connection closed.")
---
+++ synpuf1/etl/pipeline.py
@@ -0,0 +1,52 @@
+import subprocess
+import os
+import time
+import dotenv
+
+dotenv.load_dotenv(".env")
+
+def main():
+    # Ask the user for the files to extract. Keep asking until the user enters a valid input
+    files_to_extract = input("Which files? (Enter any number(s) between 1 and 20, or 'all'): ")
+    files_to_extract = files_to_extract.strip().split()
+
+    while not all(map(str.isdigit, files_to_extract)) and files_to_extract != ["all"] and files_to_extract != ["exit"]:
+        files_to_extract = input("Invalid argument. Please enter either any number(s) between 1 and 20, separated by spaces, or the word 'all', or type 'exit' to escape: ")
+        files_to_extract = files_to_extract.strip().split()
+
+    if files_to_extract == ["exit"]:
+        return
+
+    if files_to_extract == ["all"] or (all(map(str.isdigit, files_to_extract)) and all(map(lambda x: 1 <= int(x) <= 20, files_to_extract))):
+        # Ask the user for the load method. Keep asking until the user enters a valid input
+        load_method = input("Load method? (Enter 'overwrite' or 'append'): ")
+
+        while load_method != "overwrite" and load_method != "append" and load_method != "exit":
+            load_method = input("Invalid argument. Please enter either 'overwrite' or 'append', or type 'exit' to escape: ")
+
+        if load_method == "exit":
+            return
+        
+    # Check if the input is either "all" or any number(s) between 1 and 20. Wait to run the next file until the first file finishes.
+    if files_to_extract == ["all"]:
+        subprocess.run(["python", "/workspaces/practice/synpuf/etl/extract.py", "/workspaces/practice/synpuf/data/BASE_SYNPUF_INPUT_DIRECTORY", "all"], check=True)
+        # wait for extract.py to finish before continuing
+        while not os.path.exists(os.environ['BASE_SYNPUF_INPUT_DIRECTORY']):
+            time.sleep(1)
+        subprocess.run(["python", "/workspaces/practice/synpuf/etl/transform.py", "all"], check=True)
+    elif all(map(str.isdigit, files_to_extract)) and all(map(lambda x: 1 <= int(x) <= 20, files_to_extract)):
+        for file_number in files_to_extract:
+            # start extract.py first, then run transform.py after extract.py finishes
+            subprocess.run(["python", "/workspaces/practice/synpuf/etl/extract.py", "/workspaces/practice/synpuf/data/BASE_SYNPUF_INPUT_DIRECTORY", file_number], check=True)
+            while not os.path.exists(os.environ['BASE_SYNPUF_INPUT_DIRECTORY']):
+                time.sleep(1)
+            subprocess.run(["python", "/workspaces/practice/synpuf/etl/transform.py", file_number], check=True)
+
+    # Check if the input is either "overwrite" or "append"
+    if load_method == "overwrite" or load_method == "append":
+        subprocess.run(["python", "/workspaces/practice/synpuf/etl/load.py", load_method])
+    else:
+        return
+
+if __name__ == "__main__":
+    main()
